40e842834d17c6341f47f780d01f9832c52aa6c4
Bender11x <soaresrodrigobender@gmail.com>
2025-07-22 11:41:48 -0300

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:utilidades/src/app/app_widget.dart';
 import 'package:utilidades/src/views/counter_cubit.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
+    runApp(AppWidget());
   // runApp(AppWidget());
 
   /*
@@ -38,21 +46,6 @@ void main() {
   cubit é  uma versão mais simples e direta do bloc, sem eventos, você apenas chama métodos e emite estados diretamente
     
    */
-
-  runApp(myApp());
-
-}
-
-class myApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: BlocProvider(
-        create: (_)  => CounterCubit(),
-        child: CounterPage(),
-        ),
-    );
-  }
 
 }
 
